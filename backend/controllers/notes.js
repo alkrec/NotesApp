b0415e3bfaa4b93a6178c6ce9abda97486d8f605
Alexander Krecouzos <alex.krecouz@gmail.com>
2023-04-25 11:03:49 +0300

    updated project structure

---
+++ backend/controllers/notes.js
@@ -0,0 +1,69 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+// GET: get all notes
+notesRouter.get('/', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+
+// GET: get single note
+notesRouter.get('/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+
+// POST: create note
+notesRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+// DELETE: Delete note
+notesRouter.delete('/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+
+})
+
+
+// PUT: Update note
+notesRouter.put('/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = notesRouter
--- backend/mongo.js
+++ backend/mongo.js
@@ -1,42 +1,44 @@
-const mongoose = require('mongoose')
+//TESTING THE MONGOOSE LIBRARY
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+// const mongoose = require('mongoose')
 
-const password = process.argv[2]
+// if (process.argv.length<3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+// const password = process.argv[2]
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+// const url =
+//   `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
+// mongoose.set('strictQuery',false)
+// mongoose.connect(url)
 
-const Note = mongoose.model('Note', noteSchema)
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   date: Date,
+//   important: Boolean,
+// })
 
-const note = new Note({
-  content: 'Mongoose makes things easy',
-  date: new Date(),
-  important: true,
-})
+// const Note = mongoose.model('Note', noteSchema)
 
-/*
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
-*/
+// const note = new Note({
+//   content: 'Mongoose makes things easy',
+//   date: new Date(),
+//   important: true,
+// })
 
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
-  mongoose.connection.close()
-})
+// /*
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+// */
+
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
