76a1c32cfd6984aecc1cc15d22929991f109a6db
Alexander Krecouzos <alex.krecouz@gmail.com>
2023-04-29 10:48:39 +0300

    refactoring tests

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -2,29 +2,55 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 // GET: get all notes
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
+
+  ////  Syntax without async/await
+  // Note.find({}).then(notes => {
+  //   response.json(notes)
+  // })
 })
 
 
 // GET: get single note
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+
+  if(note) {
+    response.json(note)
+  } else {
+    response.status(404).send()
+  }
+
+  //// Syntax without express-async-errors - parameters need to be this 'async (request, response, next)'
+  // try {
+  //   const note = await Note.findById(request.params.id)
+  //   if(note) {
+  //     response.json(note)
+  //   } else {
+  //     response.status(404).send()
+  //   }
+  // } catch (exception) {
+  //   next(exception)
+  // }
+
+
+  ////  Syntax without async/await
+  // Note.findById(request.params.id)
+  //   .then(note => {
+  //     if (note) {
+  //       response.json(note)
+  //     } else {
+  //       response.status(404).end()
+  //     }
+  //   })
+  //   .catch(error => next(error))
 })
 
 
 // POST: create note
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -32,38 +58,72 @@ notesRouter.post('/', (request, response, next) => {
     important: body.important || false,
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
+
+  //// Syntax without express-async-errors - parameters need to be this 'async (request, response, next)'
+  // try {
+  //   const savedNote = await note.save()
+  //   response.status(201).json(savedNote)
+  // } catch (exception) {
+  //   next(exception)
+  // }
+
+  ////  Syntax without async/await
+  // note.save()
+  //   .then(savedNote => {
+  //     response.json(savedNote)
+  //   })
+  //   .catch(error => next(error))
 })
 
 
 // DELETE: Delete note
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+
+
+  //// Syntax without express-async-errors - parameters need to be this 'async (request, response, next)'
+  // try {
+  //   await Note.findByIdAndRemove(request.params.id)
+  //   response.status(204).end()
+  // } catch (exception) {
+  //   next(exception)
+  // }
+
+  ////  Syntax without async/await
+  // Note.findByIdAndDelete(request.params.id)
+  //   .then(() => {
+  //     response.status(204).end()
+  //   })
+  //   .catch(error => next(error))
 
 })
 
 
 // PUT: Update note
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response) => {
   const { content, important } = request.body
 
-  Note.findByIdAndUpdate(
-    request.params.id,
-    { content, important },
-    { new: true, runValidators: true, context: 'query' }
-  )
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
+  const updatedNote =
+    await Note.findByIdAndUpdate(request.params.id,
+      { content, important },
+      { new: true, runValidators: true, context: 'query' }
+    )
+
+  response.json(updatedNote)
+
+////  Syntax without async/await
+  // Note.findByIdAndUpdate(
+  //   request.params.id,
+  //   { content, important },
+  //   { new: true, runValidators: true, context: 'query' }
+  // )
+  //   .then(updatedNote => {
+  //     response.json(updatedNote)
+  //   })
+  //   .catch(error => next(error))
 })
 
 module.exports = notesRouter
---
+++ backend/tests/note_api.test.js
@@ -0,0 +1,130 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app) //wrapping the Express application from app.js creates a 'superagent' object
+
+const Note = require('../models/note')
+
+//
+// Summary: Initialize the database
+beforeEach(async () => {
+  await Note.deleteMany({}) // clear database
+  console.log('cleared')
+
+  const noteObjects = helper.initialNotes
+    .map(note => new Note(note))
+
+  const promiseArray = noteObjects.map(note => note.save())
+  await Promise.all(promiseArray)
+  // The Promise.all method can be used for transforming an array of promises into a single promise,
+  //that will be fulfilled once every promise in the array passed to it as a parameter is resolved. 
+  //The last line of code await Promise.all(promiseArray) waits until every promise for saving a note is finished, 
+  //meaning that the database has been initialized.
+
+  console.log('done')
+})
+
+
+test('notes are returned as json', async () => {
+  console.log('entered test')
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body).toHaveLength(helper.initialNotes.length)
+})
+
+test('a specific note is within the returned notes', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+  expect(contents).toContain(
+    'Browser can execute only JavaScript'
+  )
+})
+
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(resultNote.body).toEqual(noteToView)
+})
+
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToDelete = notesAtStart[0]
+
+  await api
+    .delete( `/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  expect(notesAtEnd).toHaveLength(
+    helper.initialNotes.length - 1
+  )
+
+  const contents = notesAtEnd.map(r => r.content)
+
+  expect(contents).not.toContain(noteToDelete.content)
+})
+
+
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const notesAtEnd = await helper.notesInDb()
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
+
+  const contents = notesAtEnd.map(n => n.content)
+  expect(contents).toContain(
+    'async/await simplifies making async calls'
+  )
+})
+
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
+}) //sets a timeout to 10,000, b/c received error - thrown: "Exceeded timeout of 5000 ms for a test.""
+
+
+//
+// Summary: closes the database connection
+afterAll(async () => {
+  await mongoose.connection.close()
+})
+
