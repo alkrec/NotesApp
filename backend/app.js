c8a88333f08c4541525ac263f41325886ffd1746
Alexander Krecouzos <alex.krecouz@gmail.com>
2023-05-04 13:42:17 +0300

    implemented authorization jwt tokens

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,12 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
